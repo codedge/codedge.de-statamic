44884c8a0893b446678e2d78762772963ed8a642
Holger Lösken <hl@inquence.com>
2020-12-02 16:02:09 +0100

    Adding feed

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+Route::statamic('feed', 'layouts/feed', [
+    'layout' => 'layouts/feed',
+    'content_type' => 'atom'
+]);
