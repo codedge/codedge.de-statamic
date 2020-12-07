ae771ead6ea3013cb81535b740a5fe7849060bfa
Holger Lösken <hl@inquence.com>
2020-12-07 19:30:35 +0100

    Change layout for fathom

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,6 @@ use App\Listeners\GenerateBlogPostImage;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 use Statamic\Events\EntrySaved;
 
 class EventServiceProvider extends ServiceProvider
