e1a6d1f5ff9bd6d26b907c7364e9ca6a1c0da2b8
Holger Lösken <hl@inquence.com>
2021-03-11 16:49:41 +0100

    Fix typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,49 +10,47 @@ module.exports = {
     },
     important: true,
     theme: {
-      typography: {
-        default: {
-          css: {
-            code: {
-              color: '#ff9d7d',
-              fontWeight: 'normal',
-              '&:before': {
-                display: 'none',
+        extend: {
+            colors: {
+              primary: {
+                200: "#ff9d7d",
+                400: "#ff6633",
               },
-              '&:after': {
-                display: 'none',
+              secondary: {
+                400: "#01262e"
               }
+
             },
-            a: {
-              color: '#4a5568',
-              'text-decoration': 'none',
-              '&:hover': {
-                color: '#4a5568',
-              },
+            fontFamily: {
+              sans: ["Rubik", ...defaultTheme.fontFamily.sans]
             },
-          },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        code: {
+                            color: '#ff9d7d',
+                            fontWeight: 'normal',
+                            '&:before': {
+                                display: 'none',
+                            },
+                            '&:after': {
+                                display: 'none',
+                            }
+                        },
+                        a: {
+                            color: '#4a5568',
+                            'text-decoration': 'none',
+                            '&:hover': {
+                                color: '#4a5568',
+                            },
+                        },
+                    },
+                },
+            },
         },
-      },
-      extend: {
-        colors: {
-          primary: {
-            200: "#ff9d7d",
-            400: "#ff6633",
-          },
-          secondary: {
-            400: "#01262e"
-          }
-           
-        },
-        fontFamily: {
-          sans: ["Rubik", ...defaultTheme.fontFamily.sans]
-        },
-      },
     },
     variants: {},
     plugins: [
-      require('postcss-import'),
       require('@tailwindcss/typography'),
-      require('autoprefixer')
     ],
   };
